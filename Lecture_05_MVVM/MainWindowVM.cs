6314c0c234dd6ca34d6b6f801841ccc1a70c1d2f
Bhanuka Anjana <85387483+Bhanu-2500@users.noreply.github.com>
2023-01-27 09:29:31 +0530

    init

---
+++ Lecture_05_MVVM/MainWindowVM.cs
@@ -0,0 +1,40 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lecture_05_MVVM
+{
+    public partial class MainWindowVM : ObservableObject
+    {
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        public string? firstName;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(FullName))]
+        public string? lastName;
+
+        [ObservableProperty]
+        public int age;
+
+        
+
+        public string FullName { 
+            get {
+                return $"{firstName} {lastName}";
+            } 
+        }
+
+        public void IncreaseAge()
+        {
+            age += 5;
+        }
+        public void DecreaseAge()
+        {
+            age -= 5;
+        }
+    }
+}
